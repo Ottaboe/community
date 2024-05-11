500b17736f56f3edeb486b1fa3004df059d7a72c
ottaboe <ottaboe@protonmail.com>
2024-05-11 18:52:22 +0200

    cluck always eye tracker enabled fix

--- core/modes/eyetracking_modes.py
+++ core/modes/eyetracking_modes.py
@@ -1,11 +1,12 @@
 from talon import Module, actions
 
 mod = Module()
-# Assuming the default state of the tracking control is disabled
+
+# so cluck always enables eye tracker
 
 @mod.action_class
 class Actions:
     def ensure_tracking_enabled():
-        """Ensure the tracking control is enabled."""
+        """Ensure the tracking control is disabled."""
         if not actions.tracking.control_enabled():
             actions.tracking.control_toggle()
