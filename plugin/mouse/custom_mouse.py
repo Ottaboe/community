6a5aaabdd3b211423def531b2f2ad2006f9f4f69
ottaboe <ottaboe@protonmail.com>
2024-07-08 16:57:14 +0200

    parrot middleclick + scroll

--- plugin/mouse/custom_mouse.py
+++ plugin/mouse/custom_mouse.py
@@ -8,3 +8,10 @@ class Actions:
         """Clicks the mouse left button and prints debug statement."""
         print("Executing parrot_click action")  # Debug print
         ctrl.mouse_click(button=0)  # This performs a simple left click.
+
+    def parrot_middle_click():
+        """Clicks the mouse middle button (scroll button) and prints debug statement."""
+        print("Executing parrot_middle_click action")  # Debug print
+        ctrl.mouse_click(button=2)  # This performs a middle mouse button click
+
+
