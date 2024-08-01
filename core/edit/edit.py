2ac16f334c283b6e9d816d9af3e119aabbd5fd39
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-08-01 08:09:33 +0200

    Add action `user.cut_line` to edit commands (#1523)

--- core/edit/edit.py
+++ core/edit/edit.py
@@ -169,3 +169,8 @@ class Actions:
         actions.edit.left()
         for i in range(0, half_line_length):
             actions.edit.right()
+
+    def cut_line():
+        """Cut current line"""
+        actions.edit.select_line()
+        actions.edit.cut()
